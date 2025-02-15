4d5ca326c1fd21049f3f25b0b3e53186a6ccbb13
GeorgMIH <166840281+GeorgMIH@users.noreply.github.com>
2025-02-16 00:35:41 +0700

    Add files via upload

---
+++ Exercise-2.swift
@@ -0,0 +1,12 @@
+//Don't change this
+var aYear =  Int(readLine()!)! 
+
+func isLeap(year: Int) {
+    if aYear / 4 == 0 && aYear / 100 == 0 aYear{
+        print("NO")
+    }else if aYear / 400 == 0{
+        print("YES")
+    }
+  //Write your code inside this function.
+}
+isLeap(year: 1223)
